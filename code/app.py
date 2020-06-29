4de6b76b2c987e16e7a6a5b11fe498b2e343dfc7
alexandre.pacheco <alexandrespmg@hotmail.com>
2020-06-29 14:16:04 -0300

    Add simple items

--- code/app.py
+++ code/app.py
@@ -1,14 +1,37 @@
-from flask import Flask
-from flask_restx import Resource, Api
+from flask import Flask, request
+from flask_restx import Resource, Api, abort
 
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
+items = []
+
+
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        item = self.get_item(name)
+
+        return {'item': item}, 200 if item else 404
+
+    def get_item(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return item
+
+    def post(self, name):
+        if self.get_item(name):
+            return {'message': 'An item with name {} already exists'.format(name)}, 422
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return {'item': item}, 201
 
 
-api.add_resource(Student, '/student/<string:name>')
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
-app.run()
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(debug=True)
